48bed8451ce479f61dd9499f8409dfda2637703e
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-04-05 23:23:37 +0530

    Working sfml dependency integration

--- CanIChess/CanIChess.cpp
+++ CanIChess/CanIChess.cpp
@@ -2,11 +2,23 @@
 //
 
 #include "CanIChess.h"
+#include <SFML\Window.hpp>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	sf::Window window(sf::VideoMode(800, 600), "My window");
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 	return 0;
 }
