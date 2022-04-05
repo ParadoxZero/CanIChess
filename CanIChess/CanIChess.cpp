d39f31d8dc1eedd737e5e93a7be122f9b5073ed3
Sidhin S Thomas <thomas.sidhin@outlook.com>
2022-04-05 21:53:17 +0530

    Initial Commit

---
+++ CanIChess/CanIChess.cpp
@@ -0,0 +1,12 @@
+﻿// CanIChess.cpp : Defines the entry point for the application.
+//
+
+#include "CanIChess.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
